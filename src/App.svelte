79d21940bb305b3ca83f590fc4c1c9df8f3052d6
Bronley Plumb <bronley@gmail.com>
2021-11-06 15:49:56 -0400

    Add "export to pdf" button

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
     import CalendarPage from "./CalendarPage.svelte";
     import { getHolidays } from "./util";
+
     let currentYear = new Date().getFullYear();
     let year = currentYear + 1;
     $: holidays = getHolidays(year);
     let y = currentYear - 20;
     const years = new Array(40).fill(undefined).map((x) => y++);
+    let isExportingPdf = false;
+
+    async function exportToPdf() {
+        isExportingPdf = true;
+        try {
+            const pages = [...document.querySelectorAll(".page")];
+            let pdf;
+            for (let i = 0; i < pages.length; i++) {
+                const canvas = await html2canvas(pages[i]);
+                let width = canvas.width;
+                let height = canvas.height;
+
+                if (i === 0) {
+                    //set the orientation
+                    if (width > height) {
+                        pdf = new jspdf.jsPDF("l", "px", [width, height]);
+                    } else {
+                        pdf = new jspdf.jsPDF("p", "px", [height, width]);
+                    }
+                } else {
+                    pdf.addPage();
+                }
+                //then we get the dimensions from the 'pdf' file itself
+                width = pdf.internal.pageSize.getWidth();
+                height = pdf.internal.pageSize.getHeight();
+                pdf.addImage(canvas, "JPEG", 0, 0, width, height);
+            }
+            pdf.save(`Calendar ${year}.pdf`);
+        } finally {
+            isExportingPdf = false;
+        }
+    }
 </script>
 
 <div class="main-page">
@@ -14,19 +47,27 @@
     {/each}
 </div>
 <div class="controlbar noprint">
-    <div>
-        Year:
-        <button on:click={() => year--}>&minus;</button>
-        <select bind:value={year}>
-            {#each years as loopYear}
-                <option value={loopYear}>{loopYear}</option>
-            {/each}
-        </select>
-        <button on:click={() => year++}>&plus;</button>
-    </div>
+    {#if !isExportingPdf}
+        <div style="padding: 0px 10px">
+            Year:
+            <button on:click={() => year--}>&minus;</button>
+            <select bind:value={year}>
+                {#each years as loopYear}
+                    <option value={loopYear}>{loopYear}</option>
+                {/each}
+            </select>
+            <button on:click={() => year++}>&plus;</button>
+        </div>
+        <button on:click={exportToPdf}>Export to PDF</button>
+    {:else}
+        Generating PDF. Please wait...
+    {/if}
 </div>
 
 <style>
+    button {
+        cursor: pointer;
+    }
     .main-page {
         margin: auto;
         width: 14in;
@@ -45,8 +86,8 @@
         box-shadow: 0px 14px 20px 9px black;
     }
     @media print {
-   .noprint {
-      visibility: hidden;
-   }
-}
+        .noprint {
+            visibility: hidden;
+        }
+    }
 </style>
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="svelte" />
+declare const html2canvas;
+declare const jspdf;
+declare const jsPDF;
