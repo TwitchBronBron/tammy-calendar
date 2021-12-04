55b63f9cbc0483bf2167142199fea60fb60c6585
Bronley Plumb <bronley@gmail.com>
2021-12-03 20:41:28 -0500

    chnage build time to local string

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -4,5 +4,5 @@ const filePath = path.join(__dirname, '../public/build/bundle.js');
 var file = fs.readFileSync(filePath);
 fs.writeFileSync(
     filePath,
-    file.toString().replace('VERSION_NUMBER', new Date().toISOString())
+    file.toString().replace('VERSION_NUMBER', new Date().toString())
 );
