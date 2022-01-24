865952de9a8bfc35a66e81de9d29d59e88ca3520
Bronley Plumb <bronley@gmail.com>
2022-01-24 16:54:19 -0500

    use toUTCString() for date stamp

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -4,5 +4,5 @@ const filePath = path.join(__dirname, '../public/build/bundle.js');
 var file = fs.readFileSync(filePath);
 fs.writeFileSync(
     filePath,
-    file.toString().replace('VERSION_NUMBER', new Date().toString())
+    file.toString().replace('VERSION_NUMBER', new Date().toUTCString())
 );
